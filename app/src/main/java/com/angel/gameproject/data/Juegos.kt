2a8153a11fca2c824924a426611264bcc127e08e
ghuali <guaghuali@gmail.com>
2024-12-12 11:39:27 +0000

    Hecho Juegos.kt

--- app/src/main/java/com/angel/gameproject/data/Juegos.kt
+++ app/src/main/java/com/angel/gameproject/data/Juegos.kt
@@ -1,2 +1,15 @@
 package com.angel.gameproject.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "Juegos")
+data class Juegos(
+    @PrimaryKey(autoGenerate = true) val idJuegos: Int = 0,
+    @ColumnInfo(name = "Título") val tituloJuegos: String,
+    @ColumnInfo(name = "Precio") val precio: Double,
+    val idTipojuego: Int,
+    val idPlataformas: Int
+)
