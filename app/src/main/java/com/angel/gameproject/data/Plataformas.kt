c3bd00ffb62a456ffb34f6af8bc41cdda9aee03b
ghuali <guaghuali@gmail.com>
2024-12-12 11:44:20 +0000

    Hecho Plataformas.kt

--- app/src/main/java/com/angel/gameproject/data/Plataformas.kt
+++ app/src/main/java/com/angel/gameproject/data/Plataformas.kt
@@ -1,2 +1,10 @@
 package com.angel.gameproject.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Plataformas")
+data class Plataformas(
+    @PrimaryKey(autoGenerate = true) val idPlataformas: Int = 0,
+    val tituloPlataformas: String
+)
