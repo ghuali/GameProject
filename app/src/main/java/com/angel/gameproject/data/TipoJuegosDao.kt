d9b39fa262b877a1847d3861f7d2ef23e02e5aaa
ghuali <guaghuali@gmail.com>
2024-12-12 12:10:24 +0000

    hecho PlataformasDao.kt

--- app/src/main/java/com/angel/gameproject/data/TipoJuegosDao.kt
+++ app/src/main/java/com/angel/gameproject/data/TipoJuegosDao.kt
@@ -1,4 +1,18 @@
 package com.angel.gameproject.data
 
-interface TipoJuegosDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao interface TipoJuegosDao {
+    @Insert
+    suspend fun insert(tipoJuegos: TipoJuegos)
+    @Query("SELECT * FROM tipoJuegos")
+    suspend fun getAllTipos(): List<TipoJuegos>
+    @Update
+    suspend fun update(tipoJuegos: TipoJuegos)
+    @Delete
+    suspend fun delete(tipoJuegos: TipoJuegos)
 }
