b99bc9c82c9dbc786bd1a7d21f585740b5241622
ghuali <guaghuali@gmail.com>
2024-12-12 12:06:44 +0000

    hecho JuegosDao.kt

--- app/src/main/java/com/angel/gameproject/data/JuegosDao.kt
+++ app/src/main/java/com/angel/gameproject/data/JuegosDao.kt
@@ -1,4 +1,17 @@
 package com.angel.gameproject.data
 
-interface JuegosDao {
-}
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao interface JuegosDao { @Insert suspend fun insert(juegos: Juegos)
+    @Query("SELECT * FROM juegos")
+    suspend fun getAllTasks(): List<Juegos>
+    @Update
+    suspend fun update(juegos: Juegos)
+    @Delete
+    suspend fun delete(juegos: Juegos) }
