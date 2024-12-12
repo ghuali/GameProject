ced17d6bc4121eeb5860cfe7f114cf887f416301
ghuali <guaghuali@gmail.com>
2024-12-12 12:00:40 +0000

    Cambio de logica en Juegos.kt

--- app/src/main/java/com/angel/gameproject/data/Juegos.kt
+++ app/src/main/java/com/angel/gameproject/data/Juegos.kt
@@ -2,14 +2,32 @@ package com.angel.gameproject.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 
-@Entity(tableName = "Juegos")
+@Entity(
+    tableName = "Juegos",
+    foreignKeys = [
+        ForeignKey(
+            entity = TipoJuegos::class,
+            parentColumns = ["id"],
+            childColumns = ["idTipo"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = TipoJuegos::class,
+            parentColumns = ["id"],
+            childColumns = ["idPlataformas"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Juegos(
-    @PrimaryKey(autoGenerate = true) val idJuegos: Int = 0,
-    @ColumnInfo(name = "Título") val tituloJuegos: String,
-    @ColumnInfo(name = "Precio") val precio: Double,
-    val idTipojuego: Int,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nombreJuegos: String,
+    val Precio: String,
+    val idTipo: Int,
     val idPlataformas: Int
 )
