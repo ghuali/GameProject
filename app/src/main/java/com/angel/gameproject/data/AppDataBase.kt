f16421fc34fba21d79badaee98ec5d7364564122
ghuali <guaghuali@gmail.com>
2024-12-13 10:05:13 +0000

    pegado el codigo de la bases de datos a la AppDatabase

--- app/src/main/java/com/angel/gameproject/data/AppDataBase.kt
+++ app/src/main/java/com/angel/gameproject/data/AppDataBase.kt
@@ -1,6 +1,8 @@
 package com.angel.gameproject.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Juegos::class, TipoJuegos::class,Plataformas::class], version = 1)
@@ -8,3 +10,22 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun juegosDao(): JuegosDao
     abstract fun tipojuegosDao(): TipoJuegosDao
     abstract fun plataformasDao(): PlataformasDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            // Crear una instancia única de la base de datos
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "task_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
