5742372469625e3a8f3b7407df2118f2fb035b5d
ghuali <guaghuali@gmail.com>
2024-12-13 11:25:15 +0000

    cambiamos el view y añadimos Juegos en el ViewModel

--- app/src/main/java/com/angel/gameproject/view/View.kt
+++ app/src/main/java/com/angel/gameproject/view/View.kt
@@ -3,8 +3,9 @@ package com.angel.gameproject.view
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 import com.angel.gameproject.data.AppDatabase
+import com.angel.gameproject.viewModel.JuegosViewModel
 
 @Composable
-fun view(viewModel:ViewModel) {
+fun view(juegosViewModel:JuegosViewModel) {
 
 }
---
+++ app/src/main/java/com/angel/gameproject/viewModel/JuegosViewModel.kt
@@ -0,0 +1,21 @@
+package com.angel.gameproject.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.angel.gameproject.data.Juegos
+import com.angel.gameproject.data.JuegosDao
+import com.angel.gameproject.data.PlataformasDao
+import com.angel.gameproject.data.TipoJuegosDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class JuegosViewModel(
+    private val juegosDao: JuegosDao,
+    private val tipoJuegosDao: TipoJuegosDao,
+    private val plataformasDao: PlataformasDao
+) : ViewModel() {
+    private val _juegos = MutableStateFlow<List<Juegos>>(emptyList())
+
+    val juegos: StateFlow<List<Juegos>> = _juegos
+
+
+}
