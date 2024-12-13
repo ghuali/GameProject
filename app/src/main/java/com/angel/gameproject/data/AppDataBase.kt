19a6d29f9d4b10ef448635d0ba17d7cab261d46d
ghuali <guaghuali@gmail.com>
2024-12-13 10:04:39 +0000

    Añadidos los Dao a la AppDatabase

--- app/src/main/java/com/angel/gameproject/data/AppDataBase.kt
+++ app/src/main/java/com/angel/gameproject/data/AppDataBase.kt
@@ -1,2 +1,10 @@
 package com.angel.gameproject.data
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Juegos::class, TipoJuegos::class,Plataformas::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun juegosDao(): JuegosDao
+    abstract fun tipojuegosDao(): TipoJuegosDao
+    abstract fun plataformasDao(): PlataformasDao
