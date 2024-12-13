632c9bac0e2d3267c17a7393b6730089ceba5f8d
ghuali <guaghuali@gmail.com>
2024-12-13 10:33:52 +0000

    cambiado un poco el view para que use el viewModel

--- app/src/main/java/com/angel/gameproject/view/View.kt
+++ app/src/main/java/com/angel/gameproject/view/View.kt
@@ -1,2 +1,10 @@
 package com.angel.gameproject.view
 
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModel
+import com.angel.gameproject.data.AppDatabase
+
+@Composable
+fun view(viewModel:ViewModel) {
+
+}
