bcb18897ce6bfddcecf4d79c6e93230eed549eb4
ghuali <guaghuali@gmail.com>
2024-12-13 12:07:51 +0000

    Empezado a implementar datos estaticos a la database

--- app/src/main/java/com/angel/gameproject/data/AppDataBase.kt
+++ app/src/main/java/com/angel/gameproject/data/AppDataBase.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [Juegos::class, TipoJuegos::class,Plataformas::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
@@ -15,17 +18,31 @@ abstract class AppDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
+        @OptIn(DelicateCoroutinesApi::class)
         fun getDatabase(context: Context): AppDatabase {
             // Crear una instancia única de la base de datos
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "task_database"
+                    "Juegos_database"
                 ).build()
                 INSTANCE = instance
+
+                GlobalScope.launch {
+                    datos(instance.tipojuegosDao(), instance.plataformasDao())
+                }
+
+
                 instance
             }
+        }
+
+        private suspend fun datos(tipoJuegosDao: TipoJuegosDao,plataformasDao: PlataformasDao) {
+
+            val tipojuegos = listOf(
+                TipoJuegos()
+            )
         }
     }
 }
