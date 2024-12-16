6c367c1abf38144796a1399522881c1e8366a26f
ghuali <guaghuali@gmail.com>
2024-12-16 14:26:44 +0000

    Creadas variables para guardar la informacion de las databases

--- app/src/main/java/com/angel/gameproject/view/View.kt
+++ app/src/main/java/com/angel/gameproject/view/View.kt
@@ -1,11 +1,20 @@
 package com.angel.gameproject.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.angel.gameproject.data.AppDatabase
 import com.angel.gameproject.viewModel.JuegosViewModel
 
 @Composable
 fun view(juegosViewModel:JuegosViewModel) {
+    val tipoJuegos by juegosViewModel.tipoJuegos.collectAsState()
+    val plataformas by juegosViewModel.plataformas.collectAsState()
+    val juegos by juegosViewModel.juegos.collectAsState()
+
 
 }
