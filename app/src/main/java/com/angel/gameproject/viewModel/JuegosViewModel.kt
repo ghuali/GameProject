db37d03d72c6c2a7b7b5aaefa8753af82468c792
ghuali <guaghuali@gmail.com>
2024-12-16 16:24:33 +0000

    Cambiado logica del ViewModel y creado el Init

--- app/src/main/java/com/angel/gameproject/viewModel/JuegosViewModel.kt
+++ app/src/main/java/com/angel/gameproject/viewModel/JuegosViewModel.kt
@@ -1,12 +1,16 @@
 package com.angel.gameproject.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.angel.gameproject.data.Juegos
 import com.angel.gameproject.data.JuegosDao
+import com.angel.gameproject.data.Plataformas
 import com.angel.gameproject.data.PlataformasDao
+import com.angel.gameproject.data.TipoJuegos
 import com.angel.gameproject.data.TipoJuegosDao
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class JuegosViewModel(
     private val juegosDao: JuegosDao,
@@ -17,9 +21,19 @@ class JuegosViewModel(
 
     val juegos: StateFlow<List<Juegos>> = _juegos
 
-    private val _tiposJuegos = MutableStateFlow<Map<Int, String>>(emptyMap())
-    val tipoJuegos: StateFlow<Map<Int, String>> = _tiposJuegos
+    private val _tiposJuegos = MutableStateFlow<List<TipoJuegos>>(emptyList())
+    val tipoJuegos: StateFlow<List<TipoJuegos>> = _tiposJuegos
 
-    private val _plataformas = MutableStateFlow<Map<Int, String>>(emptyMap())
-    val plataformas: StateFlow<Map<Int, String>> = _plataformas
+    private val _plataformas = MutableStateFlow<List<Plataformas>>(emptyList())
+    val plataformas: StateFlow<List<Plataformas>> = _plataformas
+
+    init {
+        viewModelScope.launch {
+            // Carga los tipos de juegos desde la base de datos
+            _tiposJuegos.value = tipoJuegosDao.getAllTipos()
+
+            // Carga las plataformas desde la base de datos
+            _plataformas.value = plataformasDao.getAllPlataformas()
+        }
+    }
 }
