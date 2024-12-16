47f35b58579e9c6bf7d13f8cfb6a4f2f4be12dc5
ghuali <guaghuali@gmail.com>
2024-12-16 14:56:47 +0000

    Creada Columna con texto y outlinetexfield

--- app/src/main/java/com/angel/gameproject/view/View.kt
+++ app/src/main/java/com/angel/gameproject/view/View.kt
@@ -1,11 +1,23 @@
 package com.angel.gameproject.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import com.angel.gameproject.data.AppDatabase
 import com.angel.gameproject.viewModel.JuegosViewModel
@@ -18,5 +30,20 @@ fun view(juegosViewModel:JuegosViewModel) {
 
     var newJuegosNombre by remember { mutableStateOf("") }
     var newJuegosPrecio by remember { mutableStateOf("") }
-    var
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF2196F3))
+            .padding(16.dp)
+            .padding(vertical = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Añadir juego Nuevo", fontSize = 20.sp)
+        OutlinedTextField(
+            value =
+        )
+    }
 }
